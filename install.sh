98361478eaa1af2aaf6c7242099b28a5049102b6
DevByte1328 <>
2025-03-12 14:25:19 +0200

    Sync

--- install.sh
+++ install.sh
@@ -98,7 +98,7 @@ EndSection
 KEYBOARD
 
   # Create .config directory for user 'main' and download config files
-  mkdir -p /home/main/.config
+  mkdir -p /home/main/.config/menus
   curl -o /home/main/.config/plasma-org.kde.plasma.desktop-appletsrc https://raw.githubusercontent.com/DevByte1328/arch-install/refs/heads/master/conf/plasma-org.kde.plasma.desktop-appletsrc
   curl -o /home/main/.config/plasmashellrc https://raw.githubusercontent.com/DevByte1328/arch-install/refs/heads/master/conf/plasmashellrc
   curl -o /home/main/.config/menus/applications-kmenuedit.menu https://raw.githubusercontent.com/DevByte1328/arch-install/refs/heads/master/conf/applications-kmenuedit.menu
